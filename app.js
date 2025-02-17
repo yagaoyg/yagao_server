40b057f45bae2f2249f9da301f334ad8e9446414
yagao <949899039@qq.com>
2025-02-17 21:30:49 +0800

    配置跨域中间件 表单解析中间件

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 
-app.listen(3000, () => console.log('Server listening on port 3000'))
+// 配置跨域中间件
+app.use(cors())
+
+// 配置解析表单数据的中间件
+app.use(express.urlencoded({ extended: false }))
+
+app.listen(3000, () => {
+  console.log('API server running on port 3000')
+})
